8187794418dd769124ab38eb54bfccfc9c8ab648
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-04-14 23:50:55 +0700

    upd structure

--- src/routes/(admin)/vontigo/+layout.svelte
+++ src/routes/(admin)/vontigo/+layout.svelte
@@ -6,7 +6,7 @@
 	import '@skeletonlabs/skeleton/styles/all.css';
 
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
-	import '../app.postcss';
+	// import '../../../app.postcss';
 </script>
 
 <div class="app">
