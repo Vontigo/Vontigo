429f460b07fde67c128de57978aa5bbd2cee0508
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-04-17 16:37:55 +0700

    add admin / api

---
+++ apps/admin/src/routes/(admin)/[[language=lang]]/vontigo/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/all.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	// import '../../../app.postcss';
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+</style>
---
+++ apps/admin/src/routes/(admin)/[[language=lang]]/vontigo/setup/+page.svelte
@@ -0,0 +1,65 @@
+
+<!doctype html>
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if (gte IE 9)| IEMobile |!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Vontigo Admin</title>
+
+    
+<meta name="ghost-admin/config/environment" content="%7B%22modulePrefix%22%3A%22ghost-admin%22%2C%22environment%22%3A%22production%22%2C%22rootURL%22%3A%22%22%2C%22locationType%22%3A%22trailing-hash%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%2C%22EXTEND_PROTOTYPES%22%3A%7B%22Date%22%3Afalse%2C%22Array%22%3Atrue%2C%22String%22%3Atrue%2C%22Function%22%3Afalse%7D%2C%22_APPLICATION_TEMPLATE_WRAPPER%22%3Afalse%2C%22_JQUERY_INTEGRATION%22%3Atrue%2C%22_TEMPLATE_ONLY_GLIMMER_COMPONENTS%22%3Atrue%7D%2C%22APP%22%3A%7B%22version%22%3A%225.44%22%2C%22name%22%3A%22ghost-admin%22%7D%2C%22ember-simple-auth%22%3A%7B%7D%2C%22ember-websockets%22%3A%7B%22socketIO%22%3Atrue%7D%2C%22%40sentry%2Fember%22%3A%7B%22disablePerformance%22%3Atrue%2C%22sentry%22%3A%7B%7D%7D%2C%22ember-cli-mirage%22%3A%7B%22usingProxy%22%3Afalse%2C%22useDefaultPassthroughs%22%3Atrue%7D%2C%22exportApplicationGlobal%22%3Afalse%2C%22ember-load%22%3A%7B%22loadingIndicatorClass%22%3A%22ember-load-indicator%22%7D%7D" />
+
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="MobileOptimized" content="320" />
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1, minimal-ui, viewport-fit=cover" />
+    <meta name="pinterest" content="nopin" />
+
+    <meta http-equiv="cleartype" content="on" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-title" content="Ghost" />
+
+    <link rel="shortcut icon" href="assets/img/favicon-a9c6dbdcdc3ae568f4e0dad92149a0e3.ico" />
+    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon-74680e326a7e87b159d366c7d4fb3d4b.png" />
+
+    <meta name="application-name" content="Ghost" />
+    <meta name="msapplication-TileColor" content="#15171A" />
+    <meta name="msapplication-square70x70logo" content="assets/img/small-b90396925485f17b2ca82c31be42de5f.png" />
+    <meta name="msapplication-square150x150logo" content="assets/img/medium-fef07013cffd5c45a655a250912a0ad7.png" />
+    <meta name="msapplication-square310x310logo" content="assets/img/large-ac90af7c93a4b47e8d956fa9fef31d9d.png" />
+
+    <!-- variables that we don't want postcss-custom-properties to remove -->
+    <style>
+        :root {
+            --editor-sidebar-width: 0px;
+        }
+    </style>
+
+    <link integrity="" rel="stylesheet" href="assets/vendor-3e6947aa681f0fb82b193090e520dc73.css">
+    <link integrity="" rel="stylesheet" href="assets/ghost-65a4349f579bcb6dcc5a0f0d7774f19a.css" title="light">
+
+    
+</head>
+<body>
+
+    <div class="ember-load-indicator">
+        <div class="gh-loading-content">
+            <video width="100" height="100" loop autoplay muted playsinline preload="metadata" style="width: 100px; height: 100px;">
+                <source src="assets/videos/logo-loader.mp4" type="video/mp4" />
+                <div class="gh-loading-spinner"></div>
+            </video>
+        </div>
+    </div>
+
+    
+
+    <div id="ember-basic-dropdown-wormhole"></div>
+
+    <script src="assets/vendor-f7817f9d354338ee94d5561277ea8056.js"></script>
+<script src="assets/chunk.142.4b3c5e7ebee5433ac17e.js"></script>
+<script src="assets/chunk.143.866856df015466a9d44c.js"></script>
+    <script src="assets/ghost-1ce41f5b414ff115ff8167924199e7ba.js"></script>
+</body>
+</html>
