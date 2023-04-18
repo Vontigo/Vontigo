cb76d06c7aef766e12e280be499db2f8cbcd5f63
Si Nguyen <singuyen.it@gmail.com>
2023-04-18 18:10:34 +0700

    Update post page

---
+++ apps/web/src/lib/components/CompMenuAuthors.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import IconArrowDown from '$lib/icons/IconArrowDown.svelte';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+	import { popup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+
+	let menuAuthorsValue: string;
+	let menuAuthors: PopupSettings = {
+		event: 'focus-click',
+		target: 'menuAuthors',
+		placement: 'bottom',
+		// Close the popup when the item is clicked
+		closeQuery: '.listbox-item'
+	};
+</script>
+
+<button type="button" class="btn bg-none" use:popup={menuAuthors}>
+	<span>{menuAuthorsValue ?? 'All authors'}</span>
+	<span><IconArrowDown /></span>
+</button>
+<div class="card w-48 shadow-xl py-2" data-popup="menuAuthors">
+	<!-- Listbox -->
+	<ListBox rounded="rounded-none">
+		<ListBoxItem bind:group={menuAuthorsValue} name="medium" value="All">All authors</ListBoxItem>
+	</ListBox>
+	<!-- Arrow -->
+	<div class="arrow bg-surface-100-800-token" />
+</div>
--- apps/web/src/routes/(admin)/[[language=lang]]/vontigo/posts/[type]/+page.svelte
+++ apps/web/src/routes/(admin)/[[language=lang]]/vontigo/posts/[type]/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	// import { page } from '$app/stores';
 	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	export let data;
 </script>
 
 <AppBar class="sticky top-0 z-10 p-4">
-	<h2>Dashboard</h2>
+	<h2>Posts</h2>
 </AppBar>
+
+{data.type}
