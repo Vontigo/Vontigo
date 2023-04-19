8d08c31b839152415e1683b7d568b7b05fdcc9e1
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-04-20 01:14:25 +0700

    web > themes add tag, author

---
+++ apps/web/src/routes/(front)/[[language=lang]]/[slug=postslug]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import Post from '$lib/themes/casper/Post.svelte';
+	import Page from '$lib/themes/casper/Page.svelte';
+	import { page } from '$app/stores';
+
+	export let data: PageData;
+	let post = data.post;
+</script>
+
+{#if post}
+	{#if post.content.type == 'post'}
+		<Post {post} />
+	{:else if post.content.type == 'page'}
+		<Page {post} />
+	{/if}
+{/if}
