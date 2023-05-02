ebd4a98b29c940da11ea2665c54f6f188a029402
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-02 20:47:43 +0700

    fix check admin > role

--- apps/web/src/routes/(admin)/[[language=lang]]/vontigo/+layout.server.ts
+++ apps/web/src/routes/(admin)/[[language=lang]]/vontigo/+layout.server.ts
@@ -21,7 +21,9 @@ export const load: LayoutServerLoad = async (event) => {
 
 	const session = await event.locals.getSession();
 	// if (!session?.user) throw redirect(303, '/auth/signin');
+
 	if (
+		!session?.user &&
 		session?.user.role != ENUM_USER_ROLE.Administrator &&
 		session?.user.role != ENUM_USER_ROLE.Owner
 	) {
