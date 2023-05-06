f9da5c87455288aedbf774373a350e78e42ade64
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-06 16:12:52 +0700

    admin > posts > add delete CTA

--- apps/web/src/routes/(admin)/[[language=lang]]/vontigo/posts/[type]/[[status]]/+page.svelte
+++ apps/web/src/routes/(admin)/[[language=lang]]/vontigo/posts/[type]/[[status]]/+page.svelte
@@ -295,24 +295,58 @@
 							/>
 
 							<div
-								class="ember-view permalink v-list-data v-post-list-title w-full py-4 w-full capitalize md:grid-cols-1 col-span-2 md:pb-0 flex flex-col gap-2"
+								class="ember-view permalink v-list-data v-post-list-title w-full py-4 w-full capitalize md:grid-cols-1 col-span-2 md:pb-0 flex flex-col gap-2 border-b"
 							>
 								<h3
 									class="v-content-entry-title unstyled text-sm font-medium antialiased tracking-wide flex gap-2"
 								>
 									Tags
 								</h3>
-								<CompTagsInput postId={$recordPostsDataModal.id.value} />
+								<div class="pb-4">
+									<CompTagsInput postId={$recordPostsDataModal.id.value} />
+								</div>
 							</div>
 							<div
-								class="ember-view permalink v-list-data v-post-list-title w-full py-4 w-full capitalize md:grid-cols-1 col-span-2 md:pb-0 flex flex-col gap-2"
+								class="ember-view permalink v-list-data v-post-list-title w-full py-4 w-full capitalize md:grid-cols-1 col-span-2 md:pb-0 flex flex-col gap-2 border-b"
 							>
 								<h3
 									class="v-content-entry-title unstyled text-sm font-medium antialiased tracking-wide flex gap-2"
 								>
 									Authors
 								</h3>
-								<CompAuthorsInput postId={$recordPostsDataModal.id.value} />
+								<div class="pb-4">
+									<CompAuthorsInput postId={$recordPostsDataModal.id.value} />
+								</div>
+							</div>
+							<div class="py-4 text-end">
+								<button
+									class="btn btn-sm variant-filled-primary border-none"
+									on:click={async () => {
+										const delReq = await fetch(
+											`/api/database/posts/remove/${$recordPostsDataModal.id.value}`
+										);
+										closeDrawer();
+										console.log(delReq);
+									}}
+								>
+									<span
+										><svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke-width="1.5"
+											stroke="currentColor"
+											class="w-4 h-4"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+											/>
+										</svg>
+									</span>
+									<span>Delete {$page.params.type}</span>
+								</button>
 							</div>
 						</section>
 					</div>
