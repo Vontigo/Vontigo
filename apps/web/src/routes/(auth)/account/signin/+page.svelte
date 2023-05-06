92820443c29a8b8dc53943300cb3d458e9230c56
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-07 00:58:03 +0700

    upd

--- apps/web/src/routes/(auth)/account/signin/+page.svelte
+++ apps/web/src/routes/(auth)/account/signin/+page.svelte
@@ -2,44 +2,52 @@
 	export let data;
 </script>
 
-<div class="max-w-screen-sm m-auto flex h-screen">
-	<div class="m-auto flex flex-col gap-6 px-20">
-		<h3>Hi! : )</h3>
-		<form action="/auth/callback/credentials" method="POST" class="flex flex-col gap-4">
-			<input type="hidden" name="csrfToken" value={data.csrfToken} />
-			<div>
-				<label class="section-header" for="input-username-for-credentials-provider">Email</label
-				><input
-					name="username"
-					id="input-username-for-credentials-provider"
-					type="text"
-					placeholder=""
-					label="Username"
-					class="input p-2 variant-form-material"
-				/>
+<div
+	style="
+    background: url('/bg-ai-head.gif');
+    background-size: cover;"
+>
+	<div class="backdrop-blur-sm bg-white/10">
+		<div class="max-w-screen-sm m-auto flex h-screen">
+			<div class="m-auto flex flex-col gap-6 px-20">
+				<h3>Hi! : )</h3>
+				<form action="/auth/callback/credentials" method="POST" class="flex flex-col gap-4">
+					<input type="hidden" name="csrfToken" value={data.csrfToken} />
+					<div>
+						<label class="section-header" for="input-username-for-credentials-provider">Email</label
+						><input
+							name="username"
+							id="input-username-for-credentials-provider"
+							type="text"
+							placeholder=""
+							label="Username"
+							class="input p-2 variant-form-material"
+						/>
+					</div>
+					<div>
+						<label class="section-header" for="input-password-for-credentials-provider"
+							>Password</label
+						><input
+							name="password"
+							id="input-password-for-credentials-provider"
+							type="password"
+							placeholder=""
+							label="Password"
+							class="input p-2 variant-form-material"
+						/>
+					</div>
+					<button id="submitButton" type="submit" class="btn variant-filled">Signin -></button>
+					{#if data.error}
+						<em> ⚠️ Error: {data.error} error! <br />Please try again.</em>
+					{/if}
+					<div class="text-center">
+						<a data-sveltekit-reload href="/">Back to home.</a>
+					</div>
+				</form>
 			</div>
-			<div>
-				<label class="section-header" for="input-password-for-credentials-provider">Password</label
-				><input
-					name="password"
-					id="input-password-for-credentials-provider"
-					type="password"
-					placeholder=""
-					label="Password"
-					class="input p-2 variant-form-material"
-				/>
-			</div>
-			<button id="submitButton" type="submit" class="btn variant-filled">Signin -></button>
-			{#if data.error}
-				<em> ⚠️ Error: {data.error} error! <br />Please try again.</em>
-			{/if}
-			<div class="text-center">
-				<a data-sveltekit-reload href="/">Back to home.</a>
-			</div>
-		</form>
+		</div>
 	</div>
 </div>
-
 <!-- 
 <button
 	type="button"
