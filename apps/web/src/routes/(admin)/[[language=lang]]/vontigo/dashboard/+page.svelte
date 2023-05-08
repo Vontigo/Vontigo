2ac4746204759a29024e22416a5fbf09217aed7c
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-09 01:45:00 +0700

    admin > dashboard > upd

--- apps/web/src/routes/(admin)/[[language=lang]]/vontigo/dashboard/+page.svelte
+++ apps/web/src/routes/(admin)/[[language=lang]]/vontigo/dashboard/+page.svelte
@@ -4,8 +4,8 @@
 	import { onMount } from 'svelte';
 
 	let repo: any;
-	let releases: any;
-	let contributors: any;
+	let releases: any[];
+	let contributors: any[];
 
 	onMount(async () => {
 		const reqRepo = await fetch(`https://api.github.com/repos/vontigo/vontigo`);
@@ -14,12 +14,12 @@
 		const reqReleases = await fetch(
 			`https://api.github.com/repos/vontigo/vontigo/releases?per_page=5`
 		);
-		releases = await reqReleases.json();
+		releases = (await reqReleases.json()) || [];
 
 		const reqContributors = await fetch(
 			`https://api.github.com/repos/vontigo/vontigo/contributors`
 		);
-		contributors = await reqContributors.json();
+		contributors = (await reqContributors.json()) || [];
 	});
 </script>
 
@@ -82,16 +82,11 @@
 				Releases
 			</header>
 			<section class="p-4">
-				{#await releases}
-					loading...
-				{:then items}
-					<!-- {JSON.stringify(items)} -->
-					{#if items}
-						{#each items as item}
-							{item.tag_name} <br />
-						{/each}
-					{/if}
-				{/await}
+				{#if releases?.length}
+					{#each releases as item}
+						{item.tag_name} <br />
+					{/each}
+				{/if}
 			</section>
 		</div>
 		<div class="card">
@@ -113,34 +108,31 @@
 				Contributors
 			</header>
 			<section class="p-4">
-				{#await contributors}
-					loading...
-				{:then items}
-					<!-- {JSON.stringify(items)} -->
-					{#if items}
-						{#each items as item}
-							{item.login} <br />
-						{/each}
-					{/if}
-				{/await}
+				{#if contributors?.length}
+					{#each contributors as item}
+						{item.login} <br />
+					{/each}
+				{/if}
 			</section>
 		</div>
 		<div class="card">
 			<header class="card-header uppercase flex gap-2 font-medium">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-					/>
-				</svg>
+				<span class="">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="red"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+						/>
+					</svg>
+				</span>
 
 				Sponsor
 			</header>
