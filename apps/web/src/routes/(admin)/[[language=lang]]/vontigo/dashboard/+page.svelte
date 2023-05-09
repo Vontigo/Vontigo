01c6a2125c816c0dcfb3014fb1bcb636a585e07d
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-09 11:16:57 +0700

    admin > sponsor > upd style

--- apps/web/src/routes/(admin)/[[language=lang]]/vontigo/dashboard/+page.svelte
+++ apps/web/src/routes/(admin)/[[language=lang]]/vontigo/dashboard/+page.svelte
@@ -33,7 +33,24 @@
 <div class="max-w-screen-xl mx-auto px-12 flex gap-8">
 	<div class="w-[60%] flex flex-col gap-4">
 		<div class="card">
-			<header class="card-header font-medium">Start creating content</header>
+			<header class="card-header font-medium flex gap-2">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+					/>
+				</svg>
+
+				Start creating content
+			</header>
 			<section class="p-4" />
 			<footer class="card-footer">(footer)</footer>
 		</div>
@@ -142,7 +159,11 @@
 						<span>
 							<Avatar src="/img/logo/opencollective.png" width="w-8" rounded="rounded-full" />
 						</span>
-						<a href="https://opencollective.com/vontigo" target="_blank" class="flex-auto">
+						<a
+							href="https://opencollective.com/vontigo"
+							target="_blank"
+							class="flex-auto unstyled hover:underline"
+						>
 							Open Collective
 						</a>
 					</li>
@@ -153,7 +174,7 @@
 						<a
 							href="https://crowdfunding.lfx.linuxfoundation.org/projects/vontigo"
 							target="_blank"
-							class="flex-auto"
+							class="flex-auto unstyled hover:underline"
 						>
 							The Linux Foundation
 						</a>
@@ -162,19 +183,31 @@
 						<span>
 							<Avatar src="/img/logo/patreon.png" width="w-8" rounded="rounded-full" />
 						</span>
-						<a href="https://patreon.com/vontigo" target="_blank" class="flex-auto">Patreon</a>
+						<a
+							href="https://patreon.com/vontigo"
+							target="_blank"
+							class="flex-auto unstyled hover:underline">Patreon</a
+						>
 					</li>
 					<li>
 						<span>
 							<Avatar src="/img/logo/paypal.jpg" width="w-8" rounded="rounded-full" />
 						</span>
-						<a href="https://www.paypal.me/vontigo" target="_blank" class="flex-auto">Paypal</a>
+						<a
+							href="https://www.paypal.me/vontigo"
+							target="_blank"
+							class="flex-auto unstyled hover:underline">Paypal</a
+						>
 					</li>
 					<li>
 						<span>
 							<Avatar src="/img/logo/buymeacoffee.png" width="w-8" rounded="rounded-full" /></span
 						>
-						<a href="https://www.buymeacoffee.com/vontigo" target="_blank" class="flex-auto">
+						<a
+							href="https://www.buymeacoffee.com/vontigo"
+							target="_blank"
+							class="flex-auto unstyled hover:underline"
+						>
 							Buy me a ☕
 						</a>
 					</li>
