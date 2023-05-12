34a1b84a41f1827a7f18592a67ba46e9e2f92ece
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-12 20:27:21 +0700

    remove /docs baseurl

--- apps/docs/src/routes/+layout.ts
+++ apps/docs/src/routes/+layout.ts
@@ -5,7 +5,8 @@ export const prerender = true;
 /** @type {import('./$types').LayoutLoad} */
 export const load = createKitDocsLoader({
 	sidebar: {
-		'/': null,
-		'/docs': '/docs'
+		'/': './'
+		// '/docs': '/docs',
+		// '/tutorials': '/tutorials'
 	}
 });
--- apps/docs/src/routes/+page.ts
+++ apps/docs/src/routes/+page.ts
@@ -3,5 +3,5 @@ import { redirect } from '@sveltejs/kit';
 export const prerender = true;
 
 export function load() {
-	throw redirect(307, '/docs/getting-started/introduction');
+	//throw redirect(307, '/docs/getting-started/introduction');
 }
