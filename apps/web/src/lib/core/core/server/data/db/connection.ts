ea74965a5a96a64d398185dec084d6354d80db8f
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-18 09:59:23 +0700

    db > production > add postgresql (WIP)

--- apps/web/src/lib/core/core/server/data/db/connection.ts
+++ apps/web/src/lib/core/core/server/data/db/connection.ts
@@ -1,61 +1,8 @@
-// import Database from 'better-sqlite3';
 
-// const options = { verbose: console.log };
-// export const db = new Database('./src/lib/database/vontigo.db', options);
-// db.pragma('journal_mode = WAL');
-
-// import { config } from '../../../../shared/config/env/config.json';
+import { globalConfig } from '$lib/core/shared/config/env/config';
 import knex, { Knex } from 'knex';
+// import { database } from '$lib/core/shared/config/env/config'
 
-export const knexInstance: Knex = await knex({
-	client: 'sqlite3',
-	connection: {
-		filename: './database/vontigo.db'
-	},
-	useNullAsDefault: true
-});
 
-// let knex_conn: Knex;
-// export async function connect(connection: Record<string, string>) {
-// 	let conn = {
-// 		client: 'sqlite3',
-// 		connection: {
-// 			filename: './src/lib/database/vontigo.db'
-// 		}
-// 	};
-// 	knex_conn = knex(conn);
-// 	return knex_conn;
-// }
-
-// interface KnexConfig {
-// 	[key: string]: object;
-// }
-
-// export const knexConfig: KnexConfig = {
-// 	local: {
-// 		client: 'sqlite3',
-// 		connection: {
-// 			filename: './src/lib/database/vontigo.db'
-// 		}
-// 	},
-
-// 	development: {
-// 		client: 'mysql',
-// 		connection: {
-// 			host: 'sg1-wss4.my-hosting-panel.com:3306',
-// 			user: 'vontigo',
-// 			password: 'vontigoP@ssw0rd',
-// 			database: 'vontigo'
-// 		}
-// 	},
-
-// 	production: {
-// 		client: 'mysql',
-// 		connection: {
-// 			host: 'sg1-wss4.my-hosting-panel.com:3306',
-// 			user: 'vontigo',
-// 			password: 'vontigoP@ssw0rd',
-// 			database: 'vontigo'
-// 		}
-// 	}
-// };
+export const knexInstance: Knex = await knex(globalConfig.database[process.env.NODE_ENV]);
+// export const knexInstance: Knex = await knex(globalConfig.database['production']);
