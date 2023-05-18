9799d011b7dea38c7967fd853329bd79d9196074
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-18 17:03:47 +0700

    admin > tags & user > add postgresql db

--- apps/web/src/routes/(core)/[[language=lang]]/api/admin/users/[slug]/+server.ts
+++ apps/web/src/routes/(core)/[[language=lang]]/api/admin/users/[slug]/+server.ts
@@ -1,4 +1,5 @@
 import { knexInstance } from '$lib/core/core/server/data/db/connection';
+import { typeMapping, valueMapping } from '$lib/core/core/server/helpers/database/dbHelper';
 import { ENUM_DATABASE_TABLE, ENUM_POSTS_STATUS } from '$lib/core/shared/enum.js';
 import type {
 	RreferenceStructure as ReferenceStructure,
@@ -38,25 +39,47 @@ async function getAllRows(params: any): Promise<any[] | null> {
 
 		let foreignKeyMap: any[];
 		// console.log(await knexInstance.raw('PRAGMA table_info(users);'));
-		await knexInstance.raw('PRAGMA foreign_key_list(users);').then(function (info) {
-			// foreignKeyMap = info.map(mapForeignKey);
-			// console.log(foreignKeyMap.find(key => key.from === 'created_by'));
-			foreignKeyMap = info.reduce((result, obj) => {
-				result[obj.from] = obj;
-				return result;
-			}, {});
-			// console.log(foreignKeyMap['created_by']);
-		});
+		if (!process.env.NODE_ENV === 'development') {
+			await knexInstance.raw('PRAGMA foreign_key_list(users);').then(function (info) {
+				// foreignKeyMap = info.map(mapForeignKey);
+				// console.log(foreignKeyMap.find(key => key.from === 'created_by'));
+				foreignKeyMap = info.reduce((result, obj) => {
+					result[obj.from] = obj;
+					return result;
+				}, {});
+				// console.log(foreignKeyMap['created_by']);
+			});
+		} else {
+
+			await knexInstance.raw(`SELECT conname AS "constraint_name",
+					   conrelid::regclass AS "table_name",
+					   conkey AS "column_indexes",
+					   confrelid::regclass AS "referenced_table_name",
+					   confkey AS "referenced_column_indexes",
+					   confdeltype AS "on_delete",
+					   confupdtype AS "on_update"
+				FROM pg_constraint
+				WHERE confrelid = 'users'::regclass;`)
+				.then(function (info) {
+					console.log(info);
+
+					foreignKeyMap = info.rows.reduce((result, obj) => {
+						result[obj.column_indexes[0]] = obj;
+						return result;
+					}, {});
+					// console.log(foreignKeyMap);
+				});
+		}
 
 		let row: any;
 		await knexInstance('users')
 			.columnInfo()
 			.then(function (info) {
 				// console.log(info);
-				const userstructure: TableStructure = Object.keys(info).map((key) => ({
+				const userStructure: TableStructure = Object.keys(info).map((key) => ({
 					key,
-					value: valueRows[key],
-					type: info[key].type,
+					value: valueMapping(info[key].type, valueRows[key]), //valueRows[key],
+					type: typeMapping(info[key].type),
 					maxLength: info[key].maxLength,
 					nullable: info[key].nullable,
 					defaultValue: info[key].defaultValue,
@@ -64,7 +87,7 @@ async function getAllRows(params: any): Promise<any[] | null> {
 				}));
 				// console.log(userstructure);
 
-				row = userstructure;
+				row = userStructure;
 			});
 		return row;
 	} catch (error) {
