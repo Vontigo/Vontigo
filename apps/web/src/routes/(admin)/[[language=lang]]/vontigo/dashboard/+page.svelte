94e205fe32de9fd9d7543d1eea0e5a06aed8e4f8
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-18 15:34:29 +0700

    admin > dashboard > add contributor badges

--- apps/web/src/routes/(admin)/[[language=lang]]/vontigo/dashboard/+page.svelte
+++ apps/web/src/routes/(admin)/[[language=lang]]/vontigo/dashboard/+page.svelte
@@ -5,6 +5,7 @@
 	import { adminSiteUrl } from '$lib/core/shared/stores/site';
 	import { format } from 'timeago.js';
 	import Chart from './Chart.svelte';
+	import { abbrNum } from '$lib/core/shared/utils/format/numberUtils';
 
 	let repo: any;
 	let releases: any[];
@@ -473,15 +474,45 @@
 					/>
 				</svg>
 				Contributors
+
+				<div class="flex gap-4 ml-auto">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1"
+						stroke="currentColor"
+						class="w-5 h-5"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+						/>
+					</svg>
+
+					{contributors?.data.length || ''}
+				</div>
 			</header>
-			<section class="p-4 flex gap-2">
+			<section class="p-4 flex">
 				<!-- <a href="https://github.com/vontigo/vontigo/graphs/contributors" target="_blank">
 					<img src="https://opencollective.com/vontigo/contributors.svg?width=890" />
 				</a> -->
 				{#if contributors?.data.length}
 					{#each contributors.data as item}
-						<a href={item.html_url} target="_blank">
-							<Avatar src={item.avatar_url} action={filter} actionParams="#Apollo" width="w-10" />
+						<a
+							href="https://github.com/vontigo/vontigo/graphs/contributors"
+							target="_blank"
+							title={`${item.login} - Contribution: ${item.contributions}`}
+							class="relative inline-block p-1"
+							style="opacity:{item.contributions / 100 >= 0.1 ? item.contributions / 100 : 0.1}"
+						>
+							<span
+								class="badge-icon variant-filled-warning absolute -top-0 -right-0 z-10 text-[8px]"
+							>
+								{abbrNum(item.contributions || 0)}
+							</span>
+							<Avatar src={item.avatar_url} action={filter} actionParams="#Apollo" width="w-12" />
 						</a>
 					{/each}
 				{:else}
