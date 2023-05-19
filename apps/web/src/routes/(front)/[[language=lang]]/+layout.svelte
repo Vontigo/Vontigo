d44ca582b6ed0f947dbebc426ff2491ba0b43d0d
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-19 08:19:09 +0700

    front > add loading spin

--- apps/web/src/routes/(front)/[[language=lang]]/+layout.svelte
+++ apps/web/src/routes/(front)/[[language=lang]]/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import Default from '$lib/themes/casper/Default.svelte';
 	import { PUBLIC_DEFAULT_LANG } from '$env/static/public';
-	import { page } from '$app/stores';
+	import { page, navigating } from '$app/stores';
+	import { Jumper } from 'svelte-loading-spinners';
 	import { language, origin, site, siteUrl, theme } from '$lib/core/shared/stores/site';
 	import CompAimChat from '$lib/core/core/frontend/components/shared/CompAIMChat/CompAIMChat.svelte';
 
@@ -14,9 +15,34 @@
 </script>
 
 {#key $page.url}
+	{#if $navigating}
+		<div class="barloader">
+			<Jumper />
+		</div>
+	{/if}
 	<Default>
 		<slot />
 	</Default>
 {/key}
 
 <!-- <CompAimChat /> -->
+
+<style lang="scss" global>
+	.barloader {
+		/* position: absolute;
+		top: 0;
+		width: 100%;
+		height: 10px;
+		display: flex; */
+		position: absolute;
+
+		// width: 100%;
+		height: 88px;
+		z-index: 9999;
+		margin-top: 1rem;
+		margin-left: 1rem;
+		.wrapper {
+			width: 100% !important;
+		}
+	}
+</style>
