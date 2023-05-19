3760bc4cba8f03b9e4f31fb94cc5cfd6fdd8f8c5
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-19 09:43:03 +0700

    front > theme > add meta data for post/page/index

--- apps/web/src/lib/themes/casper/Page.svelte
+++ apps/web/src/lib/themes/casper/Page.svelte
@@ -4,6 +4,7 @@
 	import { templateType } from '$lib/core/shared/stores/site';
 	import { ENUM_IMAGE_SIZE } from '$lib/core/shared/enum';
 	import IconFire from './partials/icons/IconFire.svelte';
+	import { page } from '$app/stores';
 
 	$templateType = 'page';
 	export let data: PageData;
@@ -14,6 +15,83 @@
 
 <!-- The tag above means: insert everything in this file
 into the {body} tag of the default.hbs template -->
+
+<svelte:head>
+	<meta property="og:site_name" content={$page.data.settings.site.title} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={post.title} />
+	<meta property="og:description" content={post.description} />
+	<meta property="og:url" content={$page.url} />
+	<meta property="og:image" content={$page.url.origin + post.feature_image} />
+	<meta property="article:published_time" content={post.published_at} />
+	<meta property="article:modified_time" content={post.updated_at} />
+	<meta property="article:tag" content={post.primary_tag} />
+
+	<meta property="article:publisher" content={$page.data.settings.site.facebook} />
+	<meta property="article:author" content={$page.data.settings.site.facebook} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={post.title} />
+	<meta name="twitter:description" content={post.description} />
+	<meta name="twitter:url" content={$page.url} />
+	<meta name="twitter:image" content={$page.url.origin + post.feature_image} />
+	<meta name="twitter:label1" content="Written by" />
+	<meta name="twitter:data1" content={authors.length > 0 ? authors[0]?.name : ''} />
+	<meta name="twitter:label2" content="Filed under" />
+	<meta name="twitter:data2" content={post.primary_tag} />
+	<meta name="twitter:site" content={$page.data.settings.site.twitter} />
+	<meta name="twitter:creator" content={$page.data.settings.site.twitter} />
+	<meta property="og:image:width" content="2000" />
+	<meta property="og:image:height" content="1100" />
+
+	{@html ` <script type="application/ld+json">
+		{
+			"@context": "https://schema.org",
+			"@type": "Article",
+			"publisher": {
+				"@type": "Organization",
+				"name": "${$page.data.settings.site.title}",
+				"url": "${$page.url.origin}",
+				"logo": {
+					"@type": "ImageObject",
+					"url": "${$page.url.origin + $page.data.settings.site.logo}",
+					"width": 60,
+					"height": 60
+				}
+			},
+			"author": {
+				"@type": "Person",
+				"name": "${authors.length > 0 ? authors[0]?.name : ''}",
+				"image": {
+					"@type": "ImageObject",
+					"url": "${authors.length > 0 ? authors[0]?.profile_image : ''}",
+					"width": 240,
+					"height": 240
+				},
+				"url": "${$page.url.origin + '/author/' + (authors.length > 0 ? authors[0]?.slug : '')}",
+				"sameAs": [
+					"${$page.url.origin}",
+					"${$page.data.settings.site.facebook}",
+					"${$page.data.settings.site.twitter}"
+				]
+			},
+			"headline": "${post.title}",
+			"url": "${$page.url}",
+			"datePublished": "${post.published_at}",
+			"dateModified": "${post.updated_at}",
+			"image": {
+				"@type": "ImageObject",
+				"url": "${$page.url.origin + post.feature_image}",
+				"width": 2000,
+				"height": 1100
+			},
+			"keywords": "${post.primary_tag}",
+			"description": "${post.description}",
+			"mainEntityOfPage": "${$page.url}"
+		}
+			</script>`}
+
+	{@html $page.data.settings.site.codeinjection_head}
+</svelte:head>
 
 {#if post}
 	<!-- Everything inside the #post block pulls data from the page -->
