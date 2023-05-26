3bf3606a4a1fe7a9465701689d8b3f282b83a955
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-05-27 01:40:21 +0700

    db > add my sql scripts

---
+++ apps/web/database/dump/settings_202305270126.sql
@@ -0,0 +1,162 @@
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf324de','core','last_mentions_report_email_timestamp',NULL,'number',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324df','core','db_hash','e96239f5-3723-49c2-a11c-958c0e495a3c','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324e0','core','routes_hash','3d180d52c663d173a6be791ef411ed01','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:38:05','1'),
+	 ('643c326f002137342cf324e1','core','next_update_check','1682088301','number',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:45:01','1'),
+	 ('643c326f002137342cf324e2','core','notifications','[{"dismissible":true,"location":"bottom","status":"alert","id":"643c337a002137342cf32548","createdAtVersion":"5.44.0","type":"warn","message":"Ghost is currently unable to send email. See https://ghost.org/docs/concepts/config/#mail for instructions.","seen":false,"addedAt":"2023-04-16T17:42:18.515Z"}]','array',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:42:18','1'),
+	 ('643c326f002137342cf324e3','core','version_notifications','[]','array',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324e4','core','admin_session_secret','f00dd605cbdb3218bc6a80b7aea5562544e2e0c718440441300c3c17c7fd8046','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324e5','core','theme_session_secret','6c05724b271f971223894cf1b994c8618f3427f0efa0ee45b8fe80b743bbf381','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324e6','core','vontigo_public_key','-----BEGIN RSA PUBLIC KEY-----
+MIGJAoGBAI0BSwaXvCjoSPOKcfQpQr7qiuYjCdL8Q4cgor/+355NkiRpP/NWXW7xbnhA9ddW
+n/W2km7tvgQdMggdhP3wc9r6F9MufeFZVhBoKfKiMdrLibuSsXEpHBATVFi1Gnzm0ipFCtee
+ZBJJn10N7sDHP2Gwekb0X7F9R3xno/iuVojHAgMBAAE=
+-----END RSA PUBLIC KEY-----
+','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324e7','core','vontigo_private_key','-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQCNAUsGl7wo6EjzinH0KUK+6ormIwnS/EOHIKK//t+eTZIkaT/zVl1u8W54
+QPXXVp/1tpJu7b4EHTIIHYT98HPa+hfTLn3hWVYQaCnyojHay4m7krFxKRwQE1RYtRp85tIq
+RQrXnmQSSZ9dDe7Axz9hsHpG9F+xfUd8Z6P4rlaIxwIDAQABAoGAFlStGBcxslzPKNq1H1Dl
+hCzK4JWN+b7/xsexXqyzp3NrgJ62hQgZdfeAYkUPahIK9TP7ztuR6z7gbI6YQEszXa5yutgi
+65fiwrNjy7kB1eXPeY6GOdATrCxKy26Cr/WhDKvvcYkq4ODEq48u9UU5qUUQ5wxKCD1cKzM8
+YXEjvHkCQQDjjYtfiD4KUNL9GCuG4Fy/vCv53DRu8F0lH+YaYa/8cxMpIRpiwmxGbaZS8kzd
+xv/7BlunqDsaC2Zecv+YE5PbAkEAnqHtXU7iHGxmYicvMtwc8q9DVQ7yCJgSVhhvUP6kA0h3
+AfZhfO9EyoAEhy0uFI/gGZbXnLQxj+Su+YoMBFmohQJBAM3EzjaKHgf4BIMH2zxxII3lXCLq
+hKOZSbFrgglzMs/nQ3lazOBnjb+2IcQ6BVY7QHk/y5v1XdR0YJKiDQz5DycCQQAIeWQBwQ4J
+MK1Dfw5hbCrLuBsjtxms5drM6J9Bt2ixmDRrUWshznJhUvdq9Tnp9UmmdE1uXJ3tLu09/Wbo
+8JU1AkAc9y/Pk+hb/XlSDtxsms2UB378bXSi996aahLu32pzfdTlEraeI6AfoBo9vieJMtNZ
+t5MMvX4V95viL2EZ+eWM
+-----END RSA PRIVATE KEY-----
+','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf324e8','core','members_public_key','-----BEGIN RSA PUBLIC KEY-----
+MIGJAoGBAJd7dCGZQU00ATht/PUgVQJsQ3rWJoQPQ1+7w5BualTTc6gK2ifg+wbAHBz998Su
+WgpCTMUQxmFQcqcC4bTYimtIUuLXjfGK1v3vU0ZSIUnhL8+7G0fFhSSZMcVclKWTenkm95R8
+Yps6BJkcbTlm7uf/X7LHwa/yTwT8r8mCFF35AgMBAAE=
+-----END RSA PUBLIC KEY-----
+','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324e9','core','members_private_key','-----BEGIN RSA PRIVATE KEY-----
+MIICWgIBAAKBgQCXe3QhmUFNNAE4bfz1IFUCbEN61iaED0Nfu8OQbmpU03OoCton4PsGwBwc
+/ffErloKQkzFEMZhUHKnAuG02IprSFLi143xitb971NGUiFJ4S/PuxtHxYUkmTHFXJSlk3p5
+JveUfGKbOgSZHG05Zu7n/1+yx8Gv8k8E/K/JghRd+QIDAQABAoGAAa9c3lbPFCXE8bS/gJaI
+e4tqwL4LFaL50+HJzhx8UE891Ruxy4nEIRIX/faRP/3UgJK2Oeby3fhAHj1490MFXyGgEqmZ
+IK5U5gEWAZnEGMK2ZJ4EfCa44i5YGvADcXBq/F6FYdA6G4O1PlWoHa3Ptt2GEAKtmMXQSZsR
+2z2McwUCQQD6Gq6O9lkZApYNsYMzm61zSq9eX9b5wMQe7cAGZYr7nJVmJJImTveFPkHOFc4j
+gX/I8bdDIelZ8A7VT2C+W6UzAkEAmw2csgQB+/xSHVOP7dDtQ7N/CGqc+Fpb1R3++VeuIbnL
+WEOdsFqqcW+WjQq/N7NnE1aeMvC8nlHSNZyza7AYIwJAYN8EzYrNZ8qApD0lPFAIFqfO1Rb6
+BTKMxA4CWnfqJRq5Ix73HK9pX8qFYnU/qrD19bbp7K6t25AHrsdYCWMl0QJAe5bYBZToOACh
+Ozr2CGt2E3aplP3V7KepdzAXoovfAobjrJEIyh8aqJx3dijQzgcz69go9RWqYs95LL6gzP9p
+3QI/HhFIFDmMOOyPFADU80fYH2pZKToNWgm54cgxQtmKKzlDlwOJ6/6i8JqxFtEFAd7V5p1G
+CIZewi6k+D0otKvW
+-----END RSA PRIVATE KEY-----
+','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324ea','core','members_email_auth_secret','6ec96193be0ec6685924674b1620ba5f32f2c27662065e9cbb9637d8c5d001a20631c67d199ffa9365ca75ba56e490d6dff3872cf4a5e2228cf2a6cb9d5ca4cc','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324eb','core','members_stripe_webhook_id',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324ec','core','members_stripe_webhook_secret',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324ed','site','title','Vontigo','string','PUBLIC','2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf324ee','site','description','Empower Your Content with AI: The Future-Ready Publishing Platform','string','PUBLIC','2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf324ef','site','logo','','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324f0','site','cover_image','/content/en-us/images/size/_raw/2023/04/643c326f002137342cf324f0_main__share.jpg','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:51:10','1'),
+	 ('643c326f002137342cf324f1','site','icon','','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf324f2','site','accent_color','#000000','string','PUBLIC','2023-04-16 17:37:51','1','2023-04-20 14:51:10','1'),
+	 ('643c326f002137342cf324f3','site','locale','en-us','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf324f4','site','timezone','Etc/UTC','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324f5','site','codeinjection_head','','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324f6','site','codeinjection_foot','','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324f7','site','facebook','vontigo','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf324f8','site','twitter','@vontigo','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf324f9','site','navigation','[{"label":"Home","url":"/"},{"label":"About us","url":"/about-us/"},{"label":"Collection","url":"/tag/getting-started/"},{"label":"Author","url":"/author/huy-nguyen/"}]','array',NULL,'2023-04-16 17:37:51','1','2023-04-20 15:00:01','1'),
+	 ('643c326f002137342cf324fa','site','secondary_navigation','[
+  {
+    "label": "Data & privacy",
+    "url": "/privacy/"
+  },
+  {
+    "label": "Contact",
+    "url": "/contact/"
+  },
+  {
+    "label": "Login",
+    "url": "/vontigo/"
+  },
+  {
+    "label": "Contribute →",
+    "url": "/contribute/"
+  }
+]','array',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:53:21','1'),
+	 ('643c326f002137342cf324fb','site','meta_title','Vontigo','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf324fc','site','meta_description','Empower Your Content with AI: The Future-Ready Publishing Platform','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf324fd','site','og_image',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf324fe','site','og_title','Vontigo','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf324ff','site','og_description','Empower Your Content with AI: The Future-Ready Publishing Platform','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf32500','site','twitter_image',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32501','site','twitter_title','Vontigo','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf32502','site','twitter_description','Empower Your Content with AI: The Future-Ready Publishing Platform','string',NULL,'2023-04-16 17:37:51','1','2023-04-20 14:46:30','1'),
+	 ('643c326f002137342cf32503','theme','active_theme','casper','string','RO','2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32504','private','is_private','false','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32505','private','password','','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf32506','private','public_hash','c2775860ae130f4466bd73089ced0d','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32507','members','default_content_visibility','public','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32508','members','default_content_visibility_tiers','[]','array',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32509','members','members_signup_access','invite','string',NULL,'2023-04-16 17:37:51','1','2023-04-18 05:15:02','1'),
+	 ('643c326f002137342cf3250a','members','members_support_address','noreply','string','PUBLIC,RO','2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3250b','members','stripe_secret_key',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3250c','members','stripe_publishable_key',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3250d','members','stripe_plans','[]','array',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3250e','members','stripe_connect_publishable_key',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3250f','members','stripe_connect_secret_key',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf32510','members','stripe_connect_livemode',NULL,'boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32511','members','stripe_connect_display_name',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32512','members','stripe_connect_account_id',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32513','members','members_monthly_price_id',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32514','members','members_yearly_price_id',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32515','members','members_track_sources','true','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32516','portal','portal_name','true','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32517','portal','portal_button','true','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32518','portal','portal_plans','["free"]','array',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32519','portal','portal_products','[]','array',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf3251a','portal','portal_button_style','icon-and-text','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3251b','portal','portal_button_icon',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3251c','portal','portal_button_signup_text','Subscribe','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3251d','portal','portal_signup_terms_html',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3251e','portal','portal_signup_checkbox_required','false','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3251f','email','mailgun_domain',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32520','email','mailgun_api_key',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32521','email','mailgun_base_url',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32522','email','email_track_opens','true','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32523','email','email_track_clicks','true','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf32524','email','email_verification_required','false','boolean','RO','2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32525','amp','amp','false','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32526','amp','amp_gtag_id',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32527','firstpromoter','firstpromoter','false','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32528','firstpromoter','firstpromoter_id',NULL,'string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32529','labs','labs','{}','object',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3252a','slack','slack_url','','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3252b','slack','slack_username','Ghost','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3252c','unsplash','unsplash','true','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3252d','views','shared_views','[]','array',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf3252e','editor','editor_default_email_recipients','visibility','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf3252f','editor','editor_default_email_recipients_filter','all','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32530','comments','comments_enabled','off','string',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32531','analytics','outbound_link_tagging','true','boolean',NULL,'2023-04-16 17:37:51','1','2023-04-16 17:37:51','1'),
+	 ('643c326f002137342cf32532','brainiacminds','aim_url','https://vontigo.services.brainiacminds.com/beta','string',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32533','brainiacminds','aim_key','YOUR_OPENAI_KEY','string',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32534','brainiacminds','aim_mainpoints','technology','string',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32535','brainiacminds','aim_topics','artificial intelligence, ai','string',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32536','brainiacminds','aim_custom_prompt','Write a high-quality article on "{{title}}" that is engaging and informative for readers. ','text',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32537','brainiacminds','aim_using_custom_prompt','false','boolean',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1');
+INSERT INTO settings (id,"group","key",value,"type",flags,created_at,created_by,updated_at,updated_by) VALUES
+	 ('643c326f002137342cf32538','brainiacminds','aim_temperature','0.1','number',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32539','brainiacminds','aim_max_tokens','1000','number',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32540','brainiacminds','aim_article_max_words','250','number',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32541','google','iam_service_account_client_email','XXX.iam.gserviceaccount.com','string',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32542','google','iam_service_account_private_key','-----BEGIN PRIVATE...','string',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32543','google','analytics_measurement_id','G-1VTZ4YXXXX','string',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1'),
+	 ('643c326f002137342cf32544','google','analytics_view_id','189586732','string',NULL,'2023-05-08 17:37:51','1','2023-05-08 17:37:51','1');
