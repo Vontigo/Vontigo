86d5de679e5661d1f6cd32a33686e31f70f9fe43
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-08-09 01:59:24 +0700

    add revogrid

--- apps/web/src/routes/(admin)/[[language=lang]]/vontigo/[[app]]/base/+page.svelte
+++ apps/web/src/routes/(admin)/[[language=lang]]/vontigo/[[app]]/base/+page.svelte
@@ -1,1 +1,29 @@
-Base
+<script>
+	import { RevoGrid } from '@revolist/svelte-datagrid';
+	import { defineCustomElements } from '@revolist/revogrid/loader';
+	import { onMount } from 'svelte';
+
+	let source = [
+		{
+			prop: 'name',
+			name: 'First'
+		},
+		{
+			prop: 'details',
+			name: 'Second'
+		}
+	];
+	let headers = [
+		{
+			name: '1',
+			details: 'Item 1'
+		}
+	];
+
+	onMount(async () => {
+		// define webcomponent element in DOM
+		await defineCustomElements();
+	});
+</script>
+
+<RevoGrid {source} resize="true" columns={headers} theme="material" />
