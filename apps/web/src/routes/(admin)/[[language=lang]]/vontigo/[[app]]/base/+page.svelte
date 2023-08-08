862ed834f49373f340d0264c8bffb54ba35a4ff5
Huy Nguyen <Smilefounder@users.noreply.github.com>
2023-08-09 02:18:15 +0700

    upd demo table

--- apps/web/src/routes/(admin)/[[language=lang]]/vontigo/[[app]]/base/+page.svelte
+++ apps/web/src/routes/(admin)/[[language=lang]]/vontigo/[[app]]/base/+page.svelte
@@ -2,6 +2,9 @@
 	import { RevoGrid } from '@revolist/svelte-datagrid';
 	import { defineCustomElements } from '@revolist/revogrid/loader';
 	import { onMount } from 'svelte';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	import IconPlusSmall from '$lib/icons/IconPlusSmall.svelte';
 
 	const columns = [
 		{
@@ -10,27 +13,36 @@
 		},
 		{
 			prop: 'details',
-			name: 'Second',
-			cellTemplate: (createElement, props) => {
-				return createElement(
-					'div',
-					{
-						style: {
-							backgroundColor: 'gray'
-						},
-						class: {
-							'inner-cell': true
-						}
-					},
-					props.model[props.prop] || ''
-				);
-			}
+			name: 'Second'
+			// cellTemplate: (createElement, props) => {
+			// 	return createElement(
+			// 		'div',
+			// 		{
+			// 			style: {
+			// 				backgroundColor: 'gray'
+			// 			},
+			// 			class: {
+			// 				'inner-cell': true
+			// 			}
+			// 		},
+			// 		props.model[props.prop] || ''
+			// 	);
+			// }
+		},
+		{
+			prop: 'id',
+			name: 'Id'
 		}
 	];
 	const rows = [
 		{
-			name: 'New item',
+			name: 'New item 1',
 			details: 'Item description'
+		},
+		{
+			name: 'New item 2',
+			details: 'Item description',
+			id: 0
 		}
 	];
 
@@ -40,66 +52,4 @@
 	});
 </script>
 
-<!-- <RevoGrid
-		bind:this={grid}
-		source={rows}
-		resize="true"
-		columns={$columns}
-		theme="compact"
-		range
-		rowClass="id"
-		readonly
-		{rowDefinitions}
-		{editors}
-		on:aftercolumnresize={onAfterColumnResize}
-	/> -->
 <RevoGrid source={rows} resize="true" {columns} theme="compact" />
-
-<style>
-	:global(revo-grid[theme='compact'] revogr-header .header-rgRow) {
-		height: 32px;
-	}
-
-	:global(revo-grid[theme='compact'] revogr-header) {
-		line-height: 32px;
-	}
-
-	:global(revo-grid .hovered .undb-select) {
-		opacity: 1;
-	}
-
-	:global(revo-grid .hovered .undb-row-expand) {
-		opacity: 1;
-	}
-
-	:global(revo-grid .hovered .undb-row-index) {
-		display: none;
-	}
-
-	:global(revogr-header .header-rgRow) {
-		background-color: #f7f7f7;
-	}
-	:global(.dark revogr-header .header-rgRow) {
-		background-color: #374151;
-	}
-
-	:global(revogr-header .rgHeaderCell > .resizable-r) {
-		width: 2px;
-	}
-
-	:global(revogr-focus) {
-		border: 1px solid transparent;
-	}
-
-	:global(.dark .hovered) {
-		background-color: var(--primary-color) !important;
-	}
-
-	:global(.dark revogr-data .rgRow.focused-rgRow) {
-		background-color: #374151 !important;
-	}
-
-	:global(.dark revo-grid[theme='compact'] revogr-header .rgHeaderCell.focused-cell) {
-		background-color: #374151 !important;
-	}
-</style>
